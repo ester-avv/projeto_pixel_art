ad7c5090e83770d6f7ec6f772d0d412ec5637155
Ester Viana Verza <ester.av@hotmail.com>
2022-07-05 15:38:00 -0300

    Correção erros de digitação js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 function colorThePallete() {
-  let blackColor = document.getElementsByClassName('color')[0];
+  const blackColor = document.getElementsByClassName('color')[0];
   blackColor.style.backgroundColor = 'black';
 
-  let yellowColor = document.getElementsByClassName('color')[1];
-  yellowColor.style.backgroundColor = 'yellow'
+  const yellowColor = document.getElementsByClassName('color')[1];
+  yellowColor.style.backgroundColor = 'yellow';
 
-  let redColor = document.getElementsByClassName('color')[2];
-  redColor.style.backgroundColor = 'red'
+  const redColor = document.getElementsByClassName('color')[2];
+  redColor.style.backgroundColor = 'red';
 
-  let blueColor = document.getElementsByClassName('color')[3];
-  blueColor.style.backgroundColor = 'blue'
-
+  const blueColor = document.getElementsByClassName('color')[3];
+  blueColor.style.backgroundColor = 'blue';
 }
 window.onload = colorThePallete;
