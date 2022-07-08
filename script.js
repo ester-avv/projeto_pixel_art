ca4bd8789e8aab0577c20595b22cb726671238f3
Ester Viana Verza <ester.av@hotmail.com>
2022-07-08 12:40:49 -0300

    tentativa requisito 8

--- script.js
+++ script.js
@@ -18,11 +18,13 @@ function addClass() {
 }
 addClass();
 
-function changeSelected (event) {
+function changeSelected(event) {
   let firstSelected = document.getElementsByClassName('selected')[0];
   firstSelected.classList.remove('selected');
-
   event.target.classList.add('selected');
+
+  let newSelected = document.getElementsByClassName('selected')[0];
+  return newSelected;
 }
 
 let colorArray = document.querySelectorAll('.color');
@@ -30,21 +32,36 @@ console.log(colorArray);
 
 for (let index = 0; index < colorArray.length; index += 1) {
   colorArray[index].addEventListener('click', changeSelected)
+}
+
+/* let qualCor = document.querySelector('.selected');
+console.log(qualCor); */
+
+function colorPixelBlack (event){
+    let pick = document.getElementsByClassName('selected')[0];
+    let cssObj = window.getComputedStyle(pick);
+    let bgColor= cssObj.getPropertyValue('background-color');  
+    return bgColor;
+}
+
+
+function colorPixel (event){
+  if (event.target.classList.contains('pixel')){
+    let pick = document.getElementsByClassName('selected')[0];
+    let picked = window.getComputedStyle(pick).getPropertyValue('backgroundColor');
+    event.target.style.backgroundColor = picked;
   }
+}
 
-/* function changeColor() {
+let pixelArray = document.getElementsByClassName('pixel');
+for (let index = 0; index < pixelArray.length; index += 1) {
+  pixelArray[index].addEventListener('click', colorPixel)
+}
 
-  let firstSelected = document.getElementsByClassName('selected')[0];
-  firstSelected.classList.remove('selected');
-
- 
-
-} */
-
-/* changeColor(); */
 
 
 window.onload = function () {
+  colorPixelBlack();
   colorThePallete();
   addClass();
 }
